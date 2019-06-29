6a91200d8a942f6b6fac2dad54fcb1e1ded7dcdf
Mike McAllister <mike@logikbox.com>
2019-06-29 14:49:32 -0400

    Revert "node ready"
    This reverts commit 734f5e35875d633dab71303f2049a8af6ac90882.

--- dist/tardis.js
+++ dist/tardis.js
@@ -340,11 +340,10 @@ var tardis = function (theTime, pattern) {
     DayMonthDate: DayMonthDate,
     doctorwho: doctorwho
   };
-}();
-
-console.log(tardis.dateparts()); // console.log(tardis.patterned(1133481000, 'M/DD/YYYY - H:I:s TT tt'));
-
-console.log(tardis.patterned('2019-06-29T17:26:43', 'M/DD/YYYY - HH:II:SS tt')); // console.log(tardis.patterned('', 'MMMM DDDD, YYY'));
+}(); // console.log(tardis.dateparts());
+// console.log(tardis.patterned(1133481000, 'M/DD/YYYY - H:I:s TT tt'));
+// console.log(tardis.patterned('2019-06-29T17:26:43', 'M/DD/YYYY - HH:II:SS tt'));
+// console.log(tardis.patterned('', 'MMMM DDDD, YYY'));
 // console.log(tardis.patterned('', 'MMMM MMM , MM M m'));
 // console.log(tardis.patterned('', 'DDDD DDD DD, D d'));
 // console.log(tardis.patterned('', 'MMM DDD, DD YY'));
